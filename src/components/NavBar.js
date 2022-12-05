aae7774629150e907885cfd1acca35cda12feb78
BadMonkeyDev <arsenii.aivazian@gmail.com>
2022-12-05 18:28:36 +0200

    Make buttons alive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,23 +2,52 @@ import React, {useContext} from 'react';
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import {Context} from "../index";
 import {NavLink} from "react-router-dom";
-import {SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
+import {useNavigate} from 'react-router-dom'
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
+    const history = useNavigate()
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>Happy Monkey</NavLink>
                 {user.isAuth ?
                     <Nav className="ml-auto">
-                        <Button variant={'outline-light'} className='me-2'>Адміністрування</Button>
-                        <Button variant={'outline-light'} onClick={() => user.setIsAuth(false)}>Вийти</Button>
+                        <Button
+                            variant={'outline-light'}
+                            className='me-2'
+                            onClick={() => {
+                                history(ADMIN_ROUTE)
+                            }
+                        }
+                        >
+                            Адміністрування
+                        </Button>
+                        <Button
+                            variant={'outline-light'}
+                            onClick={() => {
+                                user.setIsAuth(false)
+                                history(LOGIN_ROUTE)
+                            }
+                        }
+                        >
+                            Вийти
+                        </Button>
                     </Nav>
                     :
                     <Nav className="ml-auto">
-                        <Button variant={'outline-light'} onClick={() => user.setIsAuth(true)}>Увійти</Button>
+                        <Button
+                            variant={'outline-light'}
+                            onClick={() => {
+                                user.setIsAuth(true)
+                                history(LOGIN_ROUTE)
+                            }
+                        }
+                        >
+                            Увійти
+                        </Button>
                     </Nav>
                 }
             </Container>
